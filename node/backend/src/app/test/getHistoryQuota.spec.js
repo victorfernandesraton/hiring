8e918c5f77eb282b71c10a6784c4a2804f91aac6
victorfernandesraton <vfbraton@gmail.com>
2021-07-20 19:00:47 -0300

    best improvment for testes

--- node/backend/src/app/test/getHistoryQuota.spec.js
+++ node/backend/src/app/test/getHistoryQuota.spec.js
@@ -2,49 +2,21 @@ import supertest from "supertest";
 import app from "../index.js";
 
 describe("historyQuota", () => {
-  const agent = supertest.agent(app);
-
-  test("shuld be return valid lastQuota", (done) => {
-    agent
-      .get("/stock/PETR4.SA/history?from=2017-04-04&to=2017-04-05")
-      .expect(200)
-      .then((response) => {
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("name", "PETR4.SA");
-        expect(response.body.prices).toBe([
-          {
-            opening: 14.67,
-            low: 14.57,
-            high: 14.89,
-            closing: 14.85,
-            pricedAt: "2017-04-04",
-          },
-          {
-            opening: 15.05,
-            low: 14.5,
-            high: 15.16,
-            closing: 14.57,
-            pricedAt: "2017-04-05",
-          },
-        ]);
-        done();
-      })
-      .catch((error) => {
-        expect(error).toBeUndefined();
-      });
+  test("shuld be return valid historyQUota", async () => {
+    const result = await supertest(app).get(
+      "/stocks/IBM/history?from=2017-04-04&to=2017-04-05"
+    );
+    expect(result.status).toBe(200);
+    return;
   });
 
-  test("shoud be not found stock", (done) => {
-    agent
-      .get("/stocks/hsdbshd/history?to=2021-07-03&from=2021-07-01")
-      .expect(404)
-      .then((response) => {
-        expect(response.status).toBe(404);
-        expect(response.body).toHaveProperty("message", "stock not found");
-        done();
-      })
-      .catch((error) => {
-        expect(error).toBeUndefined();
-      });
+  test("shoud be not found stock", async () => {
+    const result = await supertest(app).get(
+      "/stocks/edede/history?to=2021-07-03&from=2021-07-01"
+    );
+
+    expect(result.status).toBe(404);
+    expect(result.body.message).toBe("stock not found");
+    return;
   });
 });
--- node/backend/src/app/test/lastQuota.spec.js
+++ node/backend/src/app/test/lastQuota.spec.js
@@ -2,33 +2,17 @@ import supertest from "supertest";
 import app from "../index.js";
 
 describe("LastQuota", () => {
-  const agent = supertest.agent(app);
-
-  test("shuld be return valid lastQuota", (done) => {
-    agent
-      .get("/stocks/IBM/quote")
-      .expect(200)
-      .then((response) => {
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("name", "IBM");
-        done();
-      })
-      .catch((error) => {
-        expect(error).toBeUndefined();
-      });
+  test("shuld be return valid lastQuota", async () => {
+    const response = await supertest(app).get("/stocks/IBM/quote");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("name", "IBM");
+    return;
   });
 
-  test("shoud be not found stock", (done) => {
-    agent
-      .get("/stocks/nfdn/quote")
-      .expect(404)
-      .then((response) => {
-        expect(response.status).toBe(404);
-        expect(response.body).toHaveProperty("message", "stock not found");
-        done();
-      })
-      .catch((error) => {
-        expect(error).toBeUndefined();
-      });
+  test("shoud be not found stock", async () => {
+    const response = await supertest(app).get("/stocks/nfdn/quote");
+    expect(response.status).toBe(404);
+    expect(response.body).toHaveProperty("message", "stock not found");
+    return;
   });
 });
