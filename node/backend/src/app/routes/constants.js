3f338d5f709f1f255204cbcc2f651c3359b33df9
victorfernandesraton <vfbraton@gmail.com>
2021-07-21 02:33:31 -0300

    implement controller constants

---
+++ node/backend/src/app/routes/constants.js
@@ -0,0 +1,5 @@
+export const stcokRoutesConstants = {
+  quote: "/:stock_name/quote",
+  history: "/:stock_name/history",
+  compare: "/:stock_name/compare",
+};
--- node/backend/src/app/routes/stocks.js
+++ node/backend/src/app/routes/stocks.js
@@ -2,16 +2,22 @@ import { Router } from "express";
 import ExpressAdapter from "../../adapter/express.js";
 
 import * as stockContoller from "../controller/stocks.js";
+import { stcokRoutesConstants } from "./constants.js";
 
 const router = Router();
 
-router.get("/:stock_name/quote", ExpressAdapter(stockContoller.getLastQuota));
 router.get(
-  "/:stock_name/history",
+  stcokRoutesConstants.quote,
+  ExpressAdapter(stockContoller.getLastQuota)
+);
+
+router.get(
+  stcokRoutesConstants.history,
   ExpressAdapter(stockContoller.geHistoricalQuota)
 );
+
 router.post(
-  "/:stock_name/compare",
+  stcokRoutesConstants.compare,
   ExpressAdapter(stockContoller.getCompareStocks)
 );
 
