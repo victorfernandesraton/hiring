c12283609089e805cfd5cdb86a43df9ad29f360d
victorfernandesraton <vfbraton@gmail.com>
2021-07-22 04:17:46 -0300

    create getProjectio service

---
+++ node/backend/src/services/stock/getProjection.js
@@ -0,0 +1,55 @@
+import { parseProjectionFromDay } from "../../adapter/stock.js";
+import StockService from "./stockService.js";
+
+class StockServiceProjection extends StockService {
+  constructor({ key, uri, request }) {
+    super({ key, uri });
+    this.request = request;
+  }
+
+  async getProjections({ stockName, annount, date }) {
+    try {
+      const [responseHistory, lastQuota] = await Promise.all([
+        this.request.get(`/query`, {
+          params: {
+            function: "TIME_SERIES_DAILY_ADJUSTED",
+            symbol: stockName,
+            outputsize: "full",
+          },
+        }),
+        this.request.getLastQuota(stockName),
+      ]);
+
+      const historicalPrices = responseHistory?.data?.["Time Series (Daily)"];
+
+      if (!historicalPrices) {
+        throw new ApplicationError("stock not found", 404);
+      }
+
+      const projectionForDay = parseProjectionFromDay({
+        date,
+        historical: historicalPrices,
+      });
+
+      const capitalGainsDollar =
+        (projectionForDay.purchasedAt - lastQuota.lastPrice) * annount;
+
+      return {
+        ...projectionForDay,
+        name: stockName,
+        purchasedAmount: annount,
+        ...projectionForDay, // TODO converter pra dolar
+        lastPrice: lastQuota.lastPrice,
+        capitalGains: capitalGainsDollar,
+      };
+    } catch (error) {
+      if (error instanceof ApplicationError) {
+        throw error;
+      } else {
+        throw new ApplicationError("internal error");
+      }
+    }
+  }
+}
+
+export default StockServiceProjection;
